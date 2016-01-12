1ff2fccedd4c98f007447e5c939dc7d6b34141cb
Jaime Garcia <jgarciaqo@gmail.com>
2016-01-12 16:19:53 -0600

    init

---
+++ src/shared/menuData.js
@@ -0,0 +1,29 @@
+export default {
+  items: [{
+    title: 'Inicio',
+    url: 'inicio',
+  }, {
+    title: 'Nosotros',
+    url: 'nosotros',
+  }, {
+    title: 'Servicios',
+    url: 'servicios',
+  }, {
+    title: 'Blog',
+    url: 'blog',
+  }, {
+    title: 'Contacto',
+    url: 'contacto',
+  }],
+  icons: [{
+    title: 'facebook',
+    url: '#',
+  }],
+  addresses: [{
+    id: 'google',
+    title: 'Mountain View',
+    location: '1600 Amphitheatre Parkway Mountain View, CA 94043',
+    gmaps: '#',
+    tels: '1 650-253-0000',
+  }],
+};
--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { IndexRoute, Router, Route, Link } from 'react-router';
+import { IndexRoute, Router, Route } from 'react-router';
 
-import AppHandler from "./components/AppHandler";
-import HomeSection from "./components/sections/home";
-import AboutUsSection from "./components/sections/aboutus";
+import AppHandler from './components/AppHandler';
+import HomeSection from './components/sections/home';
+import AboutSection from './components/sections/about';
+import ContactSection from './components/sections/contact';
 
 
 export default(
   <Router>
     <Route path="/" component={AppHandler}>
       <IndexRoute component={HomeSection} />
-      <Route path="about" component={AboutUsSection} />
+      <Route path="home" component={HomeSection} />
+      <Route path="about" component={AboutSection} />
+      <Route path="contact" component={ContactSection} />
     </Route>
   </Router>
 );
