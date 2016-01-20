de9a1da7a30946b77fb4ba392a938ccf68f1ca38
Jaime <jaime@mintitmedia.com>
2016-01-19 20:58:03 -0600

    init about blocks

---
+++ src/shared/components/sections/about/block3.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Block3 extends React.Component {
+
+  render() {
+    return (<div>
+      Block3
+    </div>);
+  }
+}
+
+Block3.propTypes = {
+  data: React.PropTypes.object.isRequired,
+};
