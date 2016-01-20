f5682ca3964dffc1f1d1d76d20d38ff5ae4d5936
Jaime <jaime@mintitmedia.com>
2016-01-19 21:05:04 -0600

    init about::block3

--- src/shared/components/sections/about/block3.js
+++ src/shared/components/sections/about/block3.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import Title1 from '../../elements/titles/title1';
+import Paragraph1 from '../../elements/paragraphs/paragraph1';
+
 export default class Block3 extends React.Component {
 
   render() {
-    return (<div>
-      Block3
+    const { texts } = this.props.data;
+    return (<div className="container-fluid">
+      <Title1 className="e3">
+        {texts.text1}
+      </Title1>
+      <Paragraph1 className="d2">
+        {texts.text2}
+      </Paragraph1>
+      <Paragraph1 className="c">
+        {texts.text3}
+      </Paragraph1>
     </div>);
   }
 }
