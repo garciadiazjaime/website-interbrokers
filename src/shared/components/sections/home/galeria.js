480c0335d7a46ec09391b24ef0711b139bebc799
Jaime <jaime@mintitmedia.com>
2016-01-19 19:27:41 -0600

    fix lint

--- src/shared/components/sections/home/galeria.js
+++ src/shared/components/sections/home/galeria.js
@@ -5,7 +5,6 @@ export default class BlockA extends React.Component {
   render() {
     return (<div className="container-fluid">
         <div className="row">
-          
         </div>
       </div>);
   }
