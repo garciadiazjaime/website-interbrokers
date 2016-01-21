d272b0fe3daed11ea93307721e97082d68a41bda
Jaime <jaime@mintitmedia.com>
2016-01-21 07:27:26 -0600

    services::block1

--- src/shared/components/sections/services/data.js
+++ src/shared/components/sections/services/data.js
@@ -1,4 +1,13 @@
 /* eslint max-len: [2, 500, 4] */
 
 export default {
+  // banner principal
+  block1: {
+    texts: {
+      text1: 'SERVICIOS',
+      text2: 'Servicios integrales a ambos lados de la frontera.',
+      text3: 'Todo lo que necesitas para operar tu empresa transportista en Estados Unidos y México.',
+    },
+  },
+
 };
