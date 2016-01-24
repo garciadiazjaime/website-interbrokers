b527466701f16630c0158634e6b9468a842b58a2
alex <alejandro@mintitmedia.com>
2016-01-23 19:07:12 -0800

    bloque 1 y 3 de aboutus

--- src/shared/components/sections/about/block1.js
+++ src/shared/components/sections/about/block1.js
@@ -1,22 +1,54 @@
 import React from 'react';
 
-import Image1 from '../../elements/images/image1';
+import Wrapper1 from '../../elements/wrappers/wrapper1';
 import Title1 from '../../elements/titles/title1';
 import Paragraph1 from '../../elements/paragraphs/paragraph1';
+import Row1 from '../../elements/rows/row1';
 
 export default class Block1 extends React.Component {
 
   render() {
-    const { texts, images } = this.props.data;
-    return (<div className="container-fluid">
-      <Image1 className="" src={images.image1.src} alt={images.image1.alt} />
-      <Title1 className="h">
-        {texts.text1}
-      </Title1>
-      <Paragraph1 className="d">
-        {texts.text2}
-      </Paragraph1>
-    </div>);
+    const { texts } = this.props.data;
+    return (<Wrapper1 className="q">
+      <div className="container-fluid">
+        <Row1 className="e">
+          <div className="col-xs-12 col-sm-8">
+            <Wrapper1 className="p">
+              <Wrapper1 className="o">
+                <Title1 className="h">
+                  {texts.text1}
+                </Title1>
+                <div className="row">
+                  <div className="col-xs-10 col-sm-7">
+                    <Paragraph1 className="d">
+                      {texts.text2}
+                    </Paragraph1>
+                  </div>
+                </div>
+              </Wrapper1>
+            </Wrapper1>
+          </div>
+        </Row1>
+      </div>
+      <div className="container-fluid">
+        <div className="col-xs-12 col-sm-6">
+          <Wrapper1 className="c">
+            <Title1 className="a2">
+              {texts.text3}
+            </Title1>
+            <Paragraph1 className="a">
+              {texts.text4}
+            </Paragraph1>
+          </Wrapper1>
+        </div>
+
+        <div className="col-xs-12 col-sm-5 col-sm-offset-1">
+          <Title1 className="a3">
+            {texts.text5}
+          </Title1>
+        </div>
+      </div>
+      </Wrapper1>);
   }
 }
 
--- src/shared/components/sections/about/block3.js
+++ src/shared/components/sections/about/block3.js
@@ -2,22 +2,29 @@ import React from 'react';
 
 import Title1 from '../../elements/titles/title1';
 import Paragraph1 from '../../elements/paragraphs/paragraph1';
+import Wrapper1 from '../../elements/wrappers/wrapper1';
 
 export default class Block3 extends React.Component {
 
   render() {
     const { texts } = this.props.data;
-    return (<div className="container-fluid">
-      <Title1 className="e3">
-        {texts.text1}
-      </Title1>
-      <Paragraph1 className="d2">
-        {texts.text2}
-      </Paragraph1>
-      <Paragraph1 className="c">
-        {texts.text3}
-      </Paragraph1>
-    </div>);
+    return (<Wrapper1 className="r">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-sm-offset-3">
+            <Title1 className="e3">
+              {texts.text1}
+            </Title1>
+            <Paragraph1 className="d2">
+              {texts.text2}
+            </Paragraph1>
+            <Paragraph1 className="c2">
+              {texts.text3}
+            </Paragraph1>
+          </div>
+        </div>
+      </div>
+    </Wrapper1>);
   }
 }
 
